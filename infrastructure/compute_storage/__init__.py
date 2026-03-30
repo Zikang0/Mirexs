2582cebd464358edb71263d8538baac422f4addd
Skyopen <zikang.li@qq.com>
2026-03-30 15:03:23 +0800

    更新 __init__.py

--- infrastructure/compute_storage/__init__.py
+++ infrastructure/compute_storage/__init__.py
@@ -3,24 +3,37 @@
 负责AI模型服务、资源管理、数据存储等核心基础设施功能
 """
 
-from .model_serving_engine import ModelServingEngine
+from .model_serving_engine import (
+    ModelConfig,
+    ModelServingEngine,
+    ModelType,
+    model_serving_engine,
+)
 from .inference_optimizer import InferenceOptimizer
 from .vector_database import VectorDatabase
 from .time_series_db import TimeSeriesDatabase
 from .distributed_storage import DistributedStorage
-from .resource_manager import ResourceManager
-from .gpu_accelerator import GPUAccelerator
+from .resource_manager import ResourceAllocation, ResourceManager, ResourceRequest, ResourceType, resource_manager
+from .gpu_accelerator import GPUAccelerator, gpu_accelerator
 from .memory_allocator import MemoryAllocator
 from .cache_manager import CacheManager
 
 __all__ = [
     "ModelServingEngine",
+    "ModelConfig",
+    "ModelType",
+    "model_serving_engine",
     "InferenceOptimizer", 
     "VectorDatabase",
     "TimeSeriesDatabase",
     "DistributedStorage",
     "ResourceManager",
+    "ResourceRequest",
+    "ResourceAllocation",
+    "ResourceType",
+    "resource_manager",
     "GPUAccelerator",
+    "gpu_accelerator",
     "MemoryAllocator",
     "CacheManager"
 ]
